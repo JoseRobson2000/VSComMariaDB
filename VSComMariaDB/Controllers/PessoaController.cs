bc6eef1aa1f7dea3a48ba1bbc966f28e1ab7736d
JoseRobson2000 <129907542+JoseRobson2000@users.noreply.github.com>
2024-02-08 22:23:09 -0300

    Implementação das rotas Crud.

---
+++ VSComMariaDB/Controllers/PessoaController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VSComMariaDB.Model;
+
+namespace VSComMariaDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        /// <summary>
+        /// Pegar a lista de todas as pesoas
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<Pessoa> GetLista()
+        {
+            var _DbContext = new _DbContext();
+            var vLista = _DbContext.Pessoa.ToList();
+
+            return vLista;
+
+        }
+        /// <summary>
+        /// Pegar os dados de uma pessoa especifica
+        /// </summary>
+        /// <param name="id">ID da Pessoa</param>
+        /// <returns></returns>
+
+
+        [HttpGet("{id}")]
+        public Pessoa GetPessoa(int id)
+        {
+            //Instanciar o banco de dados
+            var _DbContext = new _DbContext();
+
+            //Selecionar a Pessoa pelo id
+            /*var vPessoa = _DbContext.Pessoa
+                  .Where(p => p.Id == id)
+                  .FirstOrDefault(); */
+
+            var vPessoa = _DbContext.Pessoa.Find(id);
+
+            //retornar dos dados
+            return vPessoa;
+        }
+
+        [HttpPost]
+        public Pessoa Inserir(Pessoa pessoa)
+        {
+            var _DbContext = new _DbContext();
+
+            _DbContext.Pessoa.Add(pessoa);
+            _DbContext.SaveChanges();
+
+
+            return pessoa;
+
+        }
+
+        [HttpPut]
+        public Pessoa Alterar(Pessoa pessoa)
+        {
+            var _DbContext = new _DbContext();
+
+            _DbContext.Pessoa.Entry(pessoa).State = EntityState.Modified;
+            _DbContext.SaveChanges();
+
+
+            return pessoa;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Remover(int id)
+        {
+            //Instanciar o banco de dados
+            var _DbContext = new _DbContext();
+
+            //Localizar o registro a ser removido pelo id
+            var vPessoa = _DbContext.Pessoa.Find(id);
+
+            //Remover o registro encontrado
+            _DbContext.Pessoa.Remove(vPessoa);
+
+            //Salvar Alterarções
+            _DbContext.SaveChanges();
+
+            return Ok();
+
+
+        }
+
+
+    }
+}
